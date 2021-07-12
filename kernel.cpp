1ec110e495b31d01f8cc7e1d71ba5964e8389fd1
manunio <max.manu.nair@gmail.com>
2021-07-12 20:42:56 +0530

    define types for supporting diff compilers

--- kernel.cpp
+++ kernel.cpp
@@ -1,22 +1,26 @@
+#include "types.h"
 
-void printf(char *str) {
-  unsigned short *VideoMemory = (unsigned short *)0xb8000;
+void printf(char* str)
+{
+    uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
-  for (int i = 0; str[i] != '\0'; ++i)
-    VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+    for(int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+
 }
 
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
-extern "C" void callConstructors() {
-  for (constructor *i = &start_ctors; i != &end_ctors; i++)
-    (*i)();
+extern "C" void callConstructors()
+{
+    for (constructor* i = &start_ctors; i != &end_ctors ; i++) 
+        (*i)();
 }
 
-extern "C" void kernelMain(void *mutiboot_structure, unsigned int magicnumber) {
-  printf("Hello World!");
+extern "C" void kernelMain(const void* mutiboot_structure, uint32_t /*multiboot_magic*/)
+{
+    printf("Hello World!");
 
-  while (1)
-    ;
+    while(1);
 }
